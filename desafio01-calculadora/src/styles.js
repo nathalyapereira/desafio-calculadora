8f9c20f2a2813d6f32278f0c01b5156bf58bc40d
Nathalya Pereira <84873348+nathalyapereira@users.noreply.github.com>
2022-12-13 14:32:23 -0300

    Update styles.js

--- desafio01-calculadora/src/styles.js
+++ desafio01-calculadora/src/styles.js
@@ -4,9 +4,7 @@ export const Container = styled.div`
     width: 100%;
     height: 100vh;
     background: rgb(131,58,180);
-background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
-    
-
+    background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
     display: flex;
     align-items: center;
     justify-content: center;
@@ -14,6 +12,7 @@ background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%,
 
 
 export const Content = styled.div`
+    display: inline-table;
     background-color: #FFFFFF;
     width: 30%;
     padding: 1rem;
